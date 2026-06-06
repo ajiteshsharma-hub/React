aed2b1d4a2eec7a9f66b486bfc1f2036e130c0f6
ajiteshsharma-hub <ajitesh2405sharma@gmail.com>
2026-06-07 00:46:17 +0530

    Third day

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading =React.createElement("h1", {id : "heading"}, "Hello World from React!");
 // root.render(heading);//This is converting the heading object into a real DOM element and rendering it on the page
 // console.log(heading); //This will return an object
 
-const parent = React.createElement("div", {id : "parent"}, [
-    React.createElement("div", {id : "child1"}, [
-        React.createElement("h1", {} , "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")
-    ]),
+// const parent = React.createElement("div", {id : "parent"}, [
+//     React.createElement("div", {id : "child1"}, [
+//         React.createElement("h1", {} , "I am h1 tag"),
+//         React.createElement("h2", {}, "I am h2 tag")
+//     ]),
     
-    React.createElement("div", {id : "child2"}, [
-        React.createElement("h1", {} , "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")
-    ])
-]);
+//     React.createElement("div", {id : "child2"}, [
+//         React.createElement("h1", {} , "I am h1 tag"),
+//         React.createElement("h2", {}, "I am h2 tag")
+//     ])
+// ]);
+
+
+const jsxHeading = (<h1 id= "heading" className = "heading">Namaste React using JSX🚀</h1> )//This code is transpiled by babel before it reaches JS Engine
+// JSX => Bable converts JSX toReact.createElement => Object => HTML element
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
